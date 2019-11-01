ae9a22a23dc9c68f9930f3b1f99a295637c53785
samlhuillier <sam.jansa123@gmail.com>
2019-11-01 21:55:28 +0000

    adding initial solver

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mastermind-solver
+
+Solve your own mastermind games using Knuth's algorithm!
---
+++ actual_solver.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <chrono>
+
+
+void set_random_seed();
+int randn(int n);
+bool is_banned(const std::vector<int>& banned_vector, int index);
+
+struct solver{
+  void give_feedback(const std::vector<int>& attempt, int& black_hits, int& white_hits){
+    //check black hits first
+    std::vector<int> banned_from_sequence;
+    black_hits = 0;
+    white_hits = 0;
+    for (int i = 0; i< length; i++){
+      if (attempt[i]==temp_sequence[i]){
+        black_hits = black_hits + 1;
+        banned_from_sequence.push_back(i);
+      }
+    }
+    std::vector<int> banned_from_attempt = banned_from_sequence;//at this point banned from attempt is the same
+
+    for (int j = 0; j < length; j++){
+      if (!is_banned(banned_from_sequence, j)){//first we check if j has been banned from black
+        //now we want to cycle through attempts
+        for (int p = 0; p<length; p++){
+          if (!is_banned(banned_from_attempt, p) && !is_banned(banned_from_sequence, j)){
+            if (temp_sequence[j]==attempt[p]){
+              white_hits = white_hits+1;
+              banned_from_attempt.push_back(p);
+              banned_from_sequence.push_back(j);
+
+            }
+          }
+        }
+      }
+    }
+
+  }
+
+    void init(int i_length, int i_num){
+        length = i_length;
+        num = i_num;
+        run_learn_before = false;
+
+        std::vector<int> temp_to_gen_all;
+        for (int g = 0; g< length; g++){
+          temp_to_gen_all.push_back(0);
+        }
+        generate_all_guesses(0, temp_to_gen_all);
+
+
+    }
+
+    void create_attempt(std::vector<int>& attempt){
+      attempt = potential_solutions[randn(potential_solutions.size())];
+    }
+
+    void learn(std::vector<int>& attempt, int black_hits, int white_hits){
+
+      int temp_black_hits = 0;
+      int temp_white_hits = 0;
+      std::vector<std::vector<int> > next_potential_solutions;
+
+      if (!run_learn_before){
+        for (int d = 0; d<length;d++){
+          temp_sequence.push_back(0);
+        }
+        run_learn_before = true;
+      }
+      for (int x = 0; x< potential_solutions.size(); x++){
+        for (int c = 0; c < potential_solutions[x].size(); c++){
+          temp_sequence[c]= potential_solutions[x][c];
+          //std::cout<<temp_sequence[c];
+        }
+        //std::cout<<std::endl;
+        give_feedback(attempt, temp_black_hits, temp_white_hits);
+        if (temp_black_hits == black_hits && temp_white_hits == white_hits){
+          next_potential_solutions.push_back(temp_sequence);
+        }
+      }
+      potential_solutions = next_potential_solutions;
+
+    }
+    void generate_all_guesses(int ind_to_change, std::vector<int> temp){
+
+      for (int i = 0; i< num; i++){
+        temp[ind_to_change]=i;
+        if (ind_to_change < length-1) {
+          generate_all_guesses(ind_to_change+1, temp);
+        }
+        else if (ind_to_change == length-1){
+          potential_solutions.push_back(temp);
+        }
+      }
+    }
+
+    int length;
+    int num;
+    bool run_learn_before;
+    std::vector<std::vector<int> > potential_solutions;
+    std::vector<int> temp_sequence;
+    int no_of_attempts;
+};
+
+int main(){
+
+  set_random_seed();
+
+    int total_attempts = 0;
+    int length;
+    int num;
+    std::cout<<"Assign a value to each color you are playing with first."<<std::endl;
+    std::cout<<"What is the length of code you are trying to find? ";
+    std::cin>>length;
+    std::cout<<"What is the total number of values each digit of the code can have? ";
+    std::cin>>num;
+    std::cout<<std::endl;
+
+    solver thisSolver;
+    thisSolver.init(length, num);
+    std::vector<int> attempt;
+
+    int black = 0;
+    int white = 0;
+    int no_of_attempts = 0;
+
+    while (black < length){
+      thisSolver.create_attempt(attempt);
+      std::cout<<"Attempt this code: ";
+      for (int i = 0; i<attempt.size(); i++){
+        std::cout<<attempt[i]<<" ";
+      }
+      std::cout<<"How many blacks did it get? ";
+      std::cin>>black;
+      std::cout<<std::endl;
+      std::cout<<"How many whites did it get? ";
+      std::cin>>white;
+      std::cout<<std::endl;
+      thisSolver.learn(attempt,black,white);
+      no_of_attempts = no_of_attempts + 1;
+    }
+    for (int i = 0; i<attempt.size(); i++){
+      std::cout<<attempt[i]<<" ";
+    }
+    std::cout<<std::endl<<"Attempts: "<<no_of_attempts<<std::endl;
+    return 0;
+}
+
+
+void set_random_seed(){
+    std::srand(std::time(0));
+}
+
+int randn(int n){
+    return std::rand() % n;
+}
+bool is_banned(const std::vector<int>& banned_vector, int index){
+  for (int i = 0; i< banned_vector.size(); i++){
+    if (banned_vector[i]== index){
+      return true;
+    }
+  }
+  return false;
+}
